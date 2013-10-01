464689fcf9be24c17a4ce174771270e951431799
Nicolas Brassard <nico@nitriques.com>
2013-10-01 12:12:00 -0400

    Added the default page implementation

---
+++ src/pages/default-page.js
@@ -0,0 +1,25 @@
+/**
+ * @author Deux Huit Huit
+ *
+ * Default page implementation
+ */
+ 
+ 
+(function ($, undefined) {
+
+	"use strict";
+	
+	var onEnter = function(next) {
+		App.callback(next);
+	};
+	
+	var init = function() {
+		
+	};
+	
+	App.pages.exports("defaultPage", {
+		init: init,
+		enter : onEnter
+	});
+	
+})(jQuery);
