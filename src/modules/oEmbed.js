d79666d121cd438c23d0d6cba6a02fe7a92bdf17
Nicolas Brassard <nico@nitriques.com>
2013-10-10 12:35:33 -0400

    Added autoplay to vimeo, make both player html5 by default

--- src/modules/oEmbed.js
+++ src/modules/oEmbed.js
@@ -32,8 +32,9 @@
 	
 	vimeoProvider = $.extend({}, abstractProvider, 
 		{
-			getIframe : function(id) {
-				return $('<iframe src="http://player.vimeo.com/video/' + id + '?autoplay=1&api=1' + '"/>');
+			getIframe : function(id, autoplay) {
+				autoplay = autoplay ? autoplay : 1;
+				return $('<iframe src="http://player.vimeo.com/video/' + id + '?autoplay=' + autoplay + '&api=1&html5=1' + '"/>');
 			},
 			
 			play : function(container) {
@@ -55,7 +56,7 @@
 			
 				var id = url.indexOf('v=') > 0 ? url.substring(url.indexOf('v=') + 2) : url.substring(url.lastIndexOf("/"));
 				autoplay = autoplay ? autoplay : 1;
-				var iframe = $('<iframe id="youtube-player-' + id + '" src="http://www.youtube.com/embed/' + id + '?feature=oembed&autoplay='+autoplay+'&enablejsapi=1&version=3' + '"/>');
+				var iframe = $('<iframe id="youtube-player-' + id + '" src="http://www.youtube.com/embed/' + id + '?feature=oembed&autoplay='+autoplay+'&enablejsapi=1&version=3&html5=1' + '"/>');
 				this._player = new window.YT.Player(iframe.get(0));
 				return iframe;
 			},
