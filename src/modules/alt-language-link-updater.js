48a0857eb0f27aa21f95a1d31f39a936be9b7825
Pascal Piche <pascal@nitriques.com>
2013-11-22 15:46:39 -0500

    Added new module : alt-language-link-updater.js
    Signed-off-by: Pascal Piche <pascal@nitriques.com>

---
+++ src/modules/alt-language-link-updater.js
@@ -0,0 +1,70 @@
+/**
+ * @author Deux Huit Huit
+ * 
+ */
+(function ($, undefined) {
+	
+	"use strict";
+	var linkSelector = '#site a.js-alt-lg-link';
+	
+	var win = $(window);
+	var linkList = {};
+	
+	var init = function () {
+		
+		//Create initial value
+		var data ={};
+		$('link[rel=alternate][hreflang]',document).each(function() {
+			var t = $(this);
+			data[t.attr('hreflang')] = t.attr('href');
+		});
+	
+		linkList[document.location.pathname] = data;
+	};
+	
+	var onPageLoaded = function(key,data,e) {
+		var linkData = {};
+		$(data.data).each(function (i, e) {  
+			if($(e).is('link')) {
+				var t = $(e);
+				if(t.attr('hreflang')) {
+					linkData[t.attr('hreflang')] = t.attr('href');
+				}
+			}
+			if($(e).is('body')) {
+				return true;
+			}
+		});
+		linkList[data.url] = linkData;
+	};
+	
+	var onEnter = function(key,data,e) {
+		if(linkList[document.location.pathname]) {
+			
+			//Update links
+			$(linkSelector).each(function(){
+				var t = $(this);
+				if(linkList[document.location.pathname][t.data('lg')]) {
+					t.attr('href',linkList[document.location.pathname][t.data('lg')]);
+				}
+			});
+		}
+	};
+	
+	var actions = {
+		pages : {
+			loaded : onPageLoaded
+		},
+		page : {
+			enter : onEnter
+		}
+	};
+	
+	var AltLanguageLinkUpdater = App.modules.exports('altLanguageLinkUpdater', {
+		init: init,
+		actions : function() {
+			return actions;
+		}
+	});
+	
+})(jQuery);
