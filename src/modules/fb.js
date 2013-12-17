020c23905194603c1393f4aed632bb645196c111
Nicolas Brassard <nico@nitriques.com>
2013-12-17 12:44:49 -0500

    -a
    Added Facebook parser module

---
+++ src/modules/fb.js
@@ -0,0 +1,37 @@
+/******************************
+ * @author Deux Huit Huit
+ ******************************/
+
+/**
+ * Facebook async parsing
+ */
+(function ($, undefined) {
+	"use strict";
+	
+	var facebookParse = function () {
+		if (!!window.FB && !!window.FB.XFBML) {
+			window.FB.XFBML.parse();
+		}
+	};
+	
+	var actions = function () {
+		return {
+			page: {
+				entering: facebookParse
+			},
+			FB: {
+				parse: facebookParse
+			}
+		};
+	};
+	
+	var init = function () {
+		facebookParse();
+	};
+	
+	var FBParser = App.modules.exports('FB', {
+		init: init,
+		actions : actions
+	});
+	
+})(jQuery);
