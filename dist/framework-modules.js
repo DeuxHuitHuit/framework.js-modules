01332615e252162f26c15fa760bb0e54f9ffb231
Nicolas Brassard <nico@nitriques.com>
2013-12-27 18:12:20 -0500

    New release

--- dist/framework-modules.js
+++ dist/framework-modules.js
@@ -1,6 +1,61 @@
-/*! framework.js-modules - v1.0.0 - 2013-12-17
+/*! framework.js-modules - v1.1.0 - - build 304 - 2013-12-27
 * https://github.com/DeuxHuitHuit/framework.js-modules
 * Copyright (c) 2013 Deux Huit Huit; Licensed MIT */
+/**
+ * @author Deux Huit Huit
+ *
+ * Search Bar
+ *
+ */
+(function ($, w, doc, undefined) {
+
+	'use strict';
+	
+	// French
+	if ($('html').attr('lang') == 'fr') {
+		jQuery.timeago.settings.strings = {
+		   // environ ~= about, it's optional
+			prefixAgo : 'Publié il y a',
+			prefixFromNow : 'd\'ici',
+			seconds : 'moins d\'une minute',
+			minute : 'environ une minute',
+			minutes : 'environ %d minutes',
+			hour : 'une heure',
+			hours : '%d heures',
+			day : 'un jour',
+			days : '%d jours',
+			month : 'un mois',
+			months : '%d mois',
+			year : 'un an',
+			years : '%d ans'
+		};
+	}
+	
+	/* Search Bar */
+	App.components.exports('timeAgo', function _searchBar() {
+		
+		var page;
+		var NB_JOURS = 30 * 24 * 60 * 60 * 1000;
+		
+		var init = function (_page) {
+			page = _page;
+			$('time.js-time-ago').each(function (i, e) {
+				var t = $(this);
+				var d = new Date(t.attr('datetime'));
+				if (new Date().getTime() - d.getTime() < NB_JOURS) {
+					t.timeago();	
+				}
+			});
+		};
+		
+		return {
+			init : init
+			
+		};
+	
+	});
+	
+})(jQuery, window, document);
 /**
  * @author Deux Huit Huit
  * 
@@ -133,6 +188,43 @@
 	});
 	
 })(jQuery);
+/******************************
+ * @author Deux Huit Huit
+ ******************************/
+
+/**
+ * Facebook async parsing
+ */
+(function ($, undefined) {
+	"use strict";
+	
+	var facebookParse = function () {
+		if (!!window.FB && !!window.FB.XFBML) {
+			window.FB.XFBML.parse();
+		}
+	};
+	
+	var actions = function () {
+		return {
+			page: {
+				entering: facebookParse
+			},
+			FB: {
+				parse: facebookParse
+			}
+		};
+	};
+	
+	var init = function () {
+		facebookParse();
+	};
+	
+	var FBParser = App.modules.exports('FB', {
+		init: init,
+		actions : actions
+	});
+	
+})(jQuery);
 /**
  * @author Deux Huit Huit
  *
@@ -255,6 +347,13 @@
  * oEmbed module
  * Supports Youtube and Vimeo APIs
  * 
+ * APIS for supported players
+ * <!-- Vimeo - Froogaloop -->
+ * <script src="//a.vimeocdn.com/js/froogaloop2.min.js"></script>
+ * <!-- Youtube iframe api -->
+ * <script src="//www.youtube.com/iframe_api"></script>
+ * <!-- Player api for dailymotion -->
+ * <script src="//api.dmcdn.net/all.js"></script>
  */
 (function ($, undefined) {
 
@@ -1122,6 +1221,82 @@
 	
 })(jQuery);
 
+/**
+ * @author Deux Huit Huit
+ * 
+ * css3 generators
+ * 
+ * Makes the use of CSS3 in javascript more easier
+ */
+
+(function ($, global, undefined) {
+	
+	'use strict';
+	
+	//var VENDOR_PREFIXES = ['', '-webkit-', '-moz-', '-o-', '-ms-'];
+	
+	// from https://github.com/DeuxHuitHuit/jQuery-Animate-Enhanced/blob/master/scripts/src/jquery.animate-enhanced.js
+	var HAS_3D =  ('WebKitCSSMatrix' in window && 'm11' in new window.WebKitCSSMatrix());
+	
+	var _getTranslation = function (x, y, z) {
+		x = !x || $.isNumeric(x) ? (x||0)+'px' : x;
+		y = !y || $.isNumeric(y) ? (y||0)+'px' : y;
+		z = !z || $.isNumeric(z) ? (z||0)+'px' : z;
+		
+		var prefix = (HAS_3D ? '3d(' : '(');
+		var suffix = (HAS_3D ? ',' + z + ')' : ')');
+		
+		return 'translate' + prefix + x + ',' + y + suffix;
+	};
+	
+	var _getRotation = function (x, y, z, theta) {
+		x = !x || $.isNumeric(x) ? (x||0) : x;
+		y = !y || $.isNumeric(y) ? (y||0) : y;
+		z = !z || $.isNumeric(z) ? (z||0) : z;
+		theta = !theta || $.isNumeric(theta) ? (theta||0)+'deg' : theta;
+		
+		var prefix = (HAS_3D ? '3d('  + x + ',' + y + ',' + z + ',' : 'Z(');
+		var suffix = (HAS_3D ? ')' : ')');
+		
+		return 'rotate' + prefix + theta + suffix;
+	};
+	
+	global.CSS3 = {
+		translate: _getTranslation,
+		rotate: _getRotation,
+		prefix: function (key, value) {
+			var c = {};
+			c[key] = value;
+			c['-webkit-'+key] = value;
+			c['-moz-'+key] = value;
+			c['-ms-'+key] = value;
+			c['-o-'+key] = value;
+			return c;
+		}
+	};
+	
+})(jQuery, window);
+/**
+ * @author Deux Huit Huit
+ *
+ * Disable/Enable plugin
+ *
+ */
+(function ($, undefined) {
+	
+	'use strict';
+	
+	var DISABLED = 'disabled';
+
+	$.fn.disable = function () {
+		$(this).attr(DISABLED, DISABLED).addClass(DISABLED);
+	};
+	
+	$.fn.enable = function () {
+		$(this).removeAttr(DISABLED).removeClass(DISABLED);
+	};
+	
+})(jQuery);
 (function ($) {
 	'use strict';
 	
