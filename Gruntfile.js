89d2ef3a36a588f22ad62ee8851f44c5c562ba2c
Nicolas Brassard <nico@nitriques.com>
2013-12-27 18:08:05 -0500

    Added the build number system

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,15 @@ module.exports = function fxGruntConfig(grunt) {
 	// VAR
 	var GRUNT_FILE = 'Gruntfile.js';
 	
-	var SRC_FOLDERS = ['./src/modules/', './src/transitions/', './src/pages/', './src/utils/'];
+	var BUILD_FILE = './dist/build.json';
+	
+	var SRC_FOLDERS = [
+		'./src/com/',
+		'./src/modules/',
+		'./src/transitions/',
+		'./src/pages/',
+		'./src/utils/'
+	];
 	var SRC_FILES = [];
 
 	// load grunt task
@@ -33,115 +41,133 @@ module.exports = function fxGruntConfig(grunt) {
 		});
 	};
 	
-	var init = function (grunt) {
-		// Project configuration.
-		grunt.initConfig({
-			pkg: grunt.file.readJSON('package.json'),
-			meta: {
-				banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-				'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-				'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+	var getBuildNumber = function () {
+		var b = {}
+		
+		try {
+			b = grunt.file.readJSON(BUILD_FILE);
+		} catch (e) {}
+		
+		b.lastBuild = b.lastBuild > 0 ? b.lastBuild + 1 : 1;
+		
+		grunt.file.write(BUILD_FILE, JSON.stringify(b));
+		
+		return b.lastBuild;
+	};
+	
+	var config = {
+		pkg: grunt.file.readJSON('package.json'),
+		build: 'auto',
+		meta: {
+			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' ++
+			'- build <%= build %> - ' +
+			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+		},
+		concat: {
+			options: {
+				process: true,
+				banner: '<%= meta.banner %>'
 			},
-			concat: {
-				options: {
-					process: true,
-					banner: '<%= meta.banner %>'
-				},
-				dist: {
-					src: SRC_FILES,
-					dest: 'dist/<%= pkg.name %>.js'
-				}
-			},
-			watch: {
-				files: SRC_FILES.concat(GRUNT_FILE),
-				tasks: ['jshint', 'complexity']
-			},
-			jshint: {
-				files: SRC_FILES.concat(GRUNT_FILE),
-				//force: true,
-				options: {
-					bitwise: true,
-					camelcase: false,
-					curly: true,
-					eqeqeq: false, // allow ==
-					forin: true,
-					//freeze: true,
-					immed: false, //
-					latedef: true, // late definition
-					newcap: true, // capitalize ctos
-					noempty: true,
-					nonew: true, // no new ..()
-					noarg: true, 
-					plusplus: false,
-					quotmark: 'single',
-					undef: true,
-					maxparams: 5,
-					maxdepth: 5,
-					maxstatements: 30,
-					maxlen: 120,
-					//maxcomplexity: 10,
-					
-					// relax options
-					//boss: true,
-					//eqnull: true, 
-					esnext: true,
-					regexp: true,
-					strict: true,
-					trailing: false,
-					sub: true, // [] notation
-					smarttabs: true,
-					lastsemic: false, // enforce semicolons
-					white: true,
-					
-					// env
-					browser: true,
-					globals: {
-						jQuery: true,
-						console: true,
-						App: true,
-						Loader: true
-					}
-				}
-			},
-			uglify: {
-				prod: {
-					files: {
-						'dist/<%= pkg.name %>.min.js': '<%= concat.dist.dest %>' 
-					}
-				},
-				options: {
-					banner: '<%= meta.banner %>',
-					sourceMap: 'dist/framework.map',
-					sourceMappingURL: 'framework.map',
-					report: 'gzip',
-					mangle: true,
-					compress: {
-						global_defs: {
-							'DEBUG': false
-						},
-						dead_code: true,
-						unused: true,
-						warnings: true
-					},
-					preserveComments: false
-				}
-			},
-			complexity: {
-				generic: {
-					src: SRC_FILES.concat(GRUNT_FILE),
-					options: {
-						//jsLintXML: 'report.xml', // create XML JSLint-like report
-						errorsOnly: false, // show only maintainability errors
-						cyclomatic: 10,
-						halstead: 25,
-						maintainability: 100
-					}
+			dist: {
+				src: SRC_FILES,
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+		},
+		watch: {
+			files: SRC_FILES.concat(GRUNT_FILE),
+			tasks: ['jshint', 'complexity']
+		},
+		jshint: {
+			files: SRC_FILES.concat(GRUNT_FILE),
+			//force: true,
+			options: {
+				bitwise: true,
+				camelcase: false,
+				curly: true,
+				eqeqeq: false, // allow ==
+				forin: true,
+				//freeze: true,
+				immed: false, //
+				latedef: true, // late definition
+				newcap: true, // capitalize ctos
+				noempty: true,
+				nonew: true, // no new ..()
+				noarg: true, 
+				plusplus: false,
+				quotmark: 'single',
+				undef: true,
+				maxparams: 5,
+				maxdepth: 5,
+				maxstatements: 30,
+				maxlen: 120,
+				//maxcomplexity: 10,
+				
+				// relax options
+				//boss: true,
+				//eqnull: true, 
+				esnext: true,
+				regexp: true,
+				strict: true,
+				trailing: false,
+				sub: true, // [] notation
+				smarttabs: true,
+				lastsemic: false, // enforce semicolons
+				white: true,
+				
+				// env
+				browser: true,
+				globals: {
+					jQuery: true,
+					console: true,
+					App: true,
+					Loader: true
 				}
 			}
-		});
-
+		},
+		uglify: {
+			prod: {
+				files: {
+					'dist/<%= pkg.name %>.min.js': '<%= concat.dist.dest %>' 
+				}
+			},
+			options: {
+				banner: '<%= meta.banner %>',
+				sourceMap: 'dist/framework.map',
+				sourceMappingURL: 'framework.map',
+				report: 'gzip',
+				mangle: true,
+				compress: {
+					global_defs: {
+						'DEBUG': false
+					},
+					dead_code: true,
+					unused: true,
+					warnings: true
+				},
+				preserveComments: false
+			}
+		},
+		complexity: {
+			generic: {
+				src: SRC_FILES.concat(GRUNT_FILE),
+				options: {
+					//jsLintXML: 'report.xml', // create XML JSLint-like report
+					errorsOnly: false, // show only maintainability errors
+					cyclomatic: 10,
+					halstead: 25,
+					maintainability: 100
+				}
+			}
+		}
+	};
+	
+	var init = function (grunt) {
+		// Project configuration.
+		grunt.initConfig();
+		
 		// Default task.
 		grunt.registerTask('default',  ['jshint', 'complexity', 'concat', 'uglify']);
 		grunt.registerTask('dev',      ['jshint', 'complexity']);
@@ -152,6 +178,8 @@ module.exports = function fxGruntConfig(grunt) {
 		loadGruntTasks(grunt);
 		
 		createSrcFiles();
+		
+		config.build = getBuildNumber();
 		
 		init(grunt);
 		
