d95dbb5e5a3a485e2461166d3a755f9666ece51e
Nicolas Brassard <nico@nitriques.com>
2013-12-27 14:42:49 -0500

    Added time-ago components

---
+++ src/com/time-ago.js
@@ -0,0 +1,55 @@
+/**
+ * @author Deux Huit Huit
+ *
+ * Search Bar
+ *
+ */
+(function ($, w, doc, undefined) {
+
+	'use strict';
+	
+	// French
+	if ($('html').attr('lang') == 'fr') {
+		jQuery.timeago.settings.strings = {
+		   // environ ~= about, it's optional
+			prefixAgo : 'Publié il y a',
+			prefixFromNow : 'd\'ici',
+			seconds : 'moins d\'une minute',
+			minute : 'environ une minute',
+			minutes : 'environ %d minutes',
+			hour : 'une heure',
+			hours : '%d heures',
+			day : 'un jour',
+			days : '%d jours',
+			month : 'un mois',
+			months : '%d mois',
+			year : 'un an',
+			years : '%d ans'
+		};
+	}
+	
+	/* Search Bar */
+	App.components.exports('timeAgo', function _searchBar() {
+		
+		var page;
+		var NB_JOURS = 30 * 24 * 60 * 60 * 1000;
+		
+		var init = function (_page) {
+			page = _page;
+			$('time.js-time-ago').each(function (i, e) {
+				var t = $(this);
+				var d = new Date(t.attr('datetime'));
+				if (new Date().getTime() - d.getTime() < NB_JOURS) {
+					t.timeago();	
+				}
+			});
+		};
+		
+		return {
+			init : init
+			
+		};
+	
+	});
+	
+})(jQuery, window, document);
