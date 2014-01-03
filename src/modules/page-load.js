6ec8d171de9e516851d2cef2e659b4257570159c
nitriques <nico@nitriques.com>
2014-01-03 15:57:47 -0500

    Final page load implementation

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -8,24 +8,90 @@
 (function ($, undefined) {
 
 	'use strict';
+	
+	var INITIAL_VALUE = 0.02; // 2%
+	var INCREMENT = 0.05; // 5%
+	var CLOSE_DELAY = 700; // ms
+	
+	var SHOW = 'show';
+	var START = 'start';
+	
+	var holder = $();
+	
+	var isStarted = false;
+	
+	var closeTimer = 0;
+	var currentValue = 0;
+	
+	var p = function (i) {
+		return (i * 100) + '%';
+	};
+	
+	var start = function () {
+		clearTimeout(closeTimer);
+		
+		currentValue = INITIAL_VALUE;
 
+		holder
+			.removeClass(START)
+			.width(p(currentValue))
+			.height();
+		holder
+			.addClass(START)
+			.addClass(SHOW);
+		
+		isStarted = true;
+		
+		App.log({args: 'Start', me: 'page-load'});
+	};
+	
+	var end = function () {
+		holder
+			.width('100%');
+		
+		closeTimer = setTimeout(function () {
+			holder.removeClass(SHOW);
+			isStarted = false;
+		}, CLOSE_DELAY);
+		
+		App.log({args: 'End', me: 'page-load'});
+	};
+	
+	var progress = function (percent) {
+		if (isStarted) {
+			var incVal = currentValue + INCREMENT;
+			currentValue = Math.max(incVal, percent);
+			holder.width(p(currentValue));
+		}
+		App.log({args: ['Progress %s', percent], me: 'page-load'});
+	};
+	
+	var loadprogress = function (key, data) {
+		progress(data.percent);
+	};
+	
 	var actions = function () {
 		return {
+			pageLoad: {
+				start: start,
+				progress: progress,
+				end: end
+			},
 			pages: {
-				loading: null,
-				loadfatalerror: null,
-				loadprogress: null,
-				loaded: null,
-				notfound: null
+				loading: start,
+				loadfatalerror: end,
+				loadprogress: loadprogress,
+				loaded: end,
+				notfound: end
 			}
 		};
 	};
 	
 	var init = function () {
-		
+		holder = $('#load-progress');
 	};
 	
-	var FBParser = App.modules.exports('page-load', {
+	var PageLoad = App.modules.exports('page-load', {
 		init: init,
 		actions : actions
 	});
