acf786f46ac6d6554786cb59a9bd51f5343f6972
Nicolas Brassard <nico@nitriques.com>
2014-01-10 14:25:55 -0500

    New module: route not found

---
+++ src/modules/route-not-found.js
@@ -0,0 +1,28 @@
+/******************************
+ * @author Deux Huit Huit
+ ******************************/
+
+/**
+ * Route not found handling
+ */
+(function ($, undefined) {
+
+	'use strict';
+	
+	var actions = function () {
+		return {
+			pages: {
+				routeNotFound: function (key, data) {
+					if (!!data && !!data.url && data.url !== document.location.pathname) {
+						document.location = data.url;
+					}
+				}
+			}
+		};
+	};
+	
+	var RouteNotFound = App.modules.exports('route-not-found', {
+		actions : actions
+	});
+	
+})(jQuery);
