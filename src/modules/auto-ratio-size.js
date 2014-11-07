d565ea93b08407c7f3243dae950e40d2e54a8ffa
Nicolas Brassard <nico@nitriques.com>
2014-11-07 18:12:51 -0500

    Added the ability to parse ratios like 16:9 or 3/4

--- src/modules/auto-ratio-size.js
+++ src/modules/auto-ratio-size.js
@@ -1,7 +1,7 @@
 /**
  * @author Deux Huit Huit
  * 
- * Window Notifier
+ * Auto ratio size
  */
 (function ($, undefined) {
 	
@@ -15,6 +15,21 @@
 		return $('> .page:visible', sitePages);
 	};
 	
+	var parseRatio = function (ratio) {
+		if (!ratio) {
+			return 1;
+		}
+		var r = ratio.toString().split(/[:\/]/i);
+		if (!r.length) {
+			return 1;
+		} else if (r.length === 1) {
+			return parseFloat(r[0]);
+		} else if (r.length === 2) {
+			return parseInt(r[0], 10) / (parseInt(r[1], 10) || 1);
+		}
+		return -1;
+	};
+	
 	var defaultCallback = function () {
 		App.mediator.notifyCurrentPage('autoRatio.resizeCompleted');
 	};
@@ -26,7 +41,7 @@
 	var updateElements = function (elements, callback) {
 		elements.each(function () {
 			var t = $(this);
-			var r = t.attr('data-auto-ratio');
+			var r =  parseRatio(t.attr('data-auto-ratio'));
 			var fx = t.attr('data-auto-ratio-property') || 'height';
 			var val;
 			
@@ -35,8 +50,9 @@
 			} else {
 				val = t.width();
 			}
+			val = ~~Math.floor(val / r);
 			
-			t.css(fx, Math.round(val / r));
+			t.css(fx, val + 'px');
 			
 			$('img[data-src-format]', t).jitImage();
 		});
