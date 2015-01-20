ecbc0448c32611d8acd1cebd9457d856c561eaec
beaubbe <beaubbe@gmail.com>
2015-01-20 15:41:27 -0500

    added page and action variable in default-page.js

--- src/pages/default-page.js
+++ src/pages/default-page.js
@@ -10,18 +10,26 @@
 	'use strict';
 	
 	App.pages.exports('defaultPage', function () {
+		var page;
 		
 		var onEnter = function (next) {
 			App.callback(next);
 		};
 		
 		var init = function () {
-			
+			page = $(this.key());
+		};
+		
+		var actions = function () {
+			return {
+				
+			};
 		};
 		
 		var self = {
 			init: init,
-			enter : onEnter
+			enter: onEnter,
+			actions: actions
 		};
 		
 		return self;
