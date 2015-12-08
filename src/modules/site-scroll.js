988221d2d406f7aee040c411e3dbfca5fcc13afd
Pascal Piche <pascal@nitriques.com>
2015-12-08 13:15:21 -0500

    Added new modules: Site scroll
    Allow to add or remove the scroll bar and fixing element margin,padding or right value with the scroll bar size.

---
+++ src/modules/site-scroll.js
@@ -0,0 +1,54 @@
+/**
+ * @author Deux Huit Huit
+ * 
+ * Site scrollBar add-remove with scrollbar size fix algo 
+ * (pad, right, margin)
+ * 
+ * Use .js-fix-scroll
+ *		-pad : Add/remove padding-right scrollbar size fix
+ * 		-right : Add/remove right scrollbar size fix
+ * 		-margin : Add/remove margin-right scrollbar size fix
+ */
+(function ($, undefined) {
+
+	'use strict';
+	
+	var win = $(window);
+	var html = $('html');
+	
+	var addScroll = function () {
+		html.removeClass('no-scroll');
+		fixScroll(0);
+	};
+	
+	var removeScroll = function () {
+		var x = win.width();
+		html.addClass('no-scroll');
+		fixScroll(win.width() - x);
+	};
+	
+	var fixScroll = function (value) {
+		$('.js-fix-scroll-pad').css({paddingRight: value});
+		$('.js-fix-scroll-right').css({right: value});
+		$('.js-fix-scroll-margin').css({marginRight: value});
+	};
+	
+	var init = function () {
+		
+	};
+	
+	var actions = function () {
+		return { 
+			site: {
+				removeScroll: removeScroll,
+				addScroll: addScroll
+			}
+		};
+	};
+
+	App.modules.exports('siteScroll', {
+		init: init,
+		actions: actions
+	});
+	
+})(jQuery);
