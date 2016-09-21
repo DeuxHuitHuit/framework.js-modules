a130acb6202ab5f2ea038c7c02952c350833e25c
Nicolas Brassard <nico@nitriques.com>
2016-09-21 11:26:16 -0400

    Added cloudflare email protection module

---
+++ src/modules/cloudflare-email.js
@@ -0,0 +1,55 @@
+/**
+ * @author Deux Huit Huit
+ * 
+ * Cloudflare email protection
+ */
+(function ($, undefined) {
+
+	'use strict';
+	var pattern = '[email protected]';
+	var doIt = function () {
+		$('a[href^="/cdn-cgi/l/email-protection"]').each(function (i, e) {
+			/* jshint ignore:start */
+			try {
+				e = $(e);
+				var a = e.attr('href').split('#')[1];
+				if (a) {
+					var j,c,s='';
+					var r=parseInt(a.substr(0,2), 16);
+					if (r) {
+						for(j=2;j < a.length;j+=2){
+							c=parseInt(a.substr(j,2), 16)^r;
+							s+=String.fromCharCode(c);
+						}
+						e.attr('href', 'mailto:' + s);
+						e.find('script').remove();
+						var span = e.find('.__cf_email__');
+						if (!!span.length) {
+							e = span;
+						}
+						if (_.string.trim(e.text()) === pattern) {
+							e.text(s);
+						}
+					}
+				}
+			} catch (e) {}
+			/* jshint ignore:end */
+		});
+	};
+
+	var actions = function () {
+		return {
+			page: {
+				enter: doIt
+			},
+			articleChanger: {
+				enter: doIt
+			}
+		};
+	};
+	
+	App.modules.exports('cloudflare-email', {
+		actions: actions
+	});
+	
+})(jQuery, window);
