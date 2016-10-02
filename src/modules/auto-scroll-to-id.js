8633e9e8281152853e970940cb32516512914c99
Nicolas Brassard <nitriques@users.noreply.github.com>
2016-10-02 18:26:06 -0400

    New component : auto-scroll-to-id (#20)

---
+++ src/modules/auto-scroll-to-id.js
@@ -0,0 +1,57 @@
+/**
+ * @author Deux Huit Huit
+ * 
+ */
+(function ($, undefined) {
+	
+	'use strict';
+	var win = $(window);
+	var site = $('#site');
+	
+	var scrollToIdClicked = function (e) {
+		
+		//Scroll To hash
+		var t = $(this);
+		var href = t.attr('href');
+		var h = href.split('#')[1];
+		var scrollCtn = null;
+		var html = $('html');
+		
+		var offsetSelector = t.attr('data-scroll-to-id-offset-selector');
+		var offset = 0;
+		var scrollCtnAttr = t.attr('data-scroll-to-id-scroll-ctn');
+		
+		if (!!scrollCtnAttr) {
+			scrollCtn = t.closest('.page').find(scrollCtnAttr);
+		}
+		
+		if (!!offsetSelector) {
+			var offsetItem = $(offsetSelector).eq(0);
+			offset = offsetItem.height() * -1;
+		}
+		
+		var target = site.find('#' + h);
+		
+		if (!!target.length) {
+			offset += target.offset().top;
+			html.velocity('scroll', { 
+				offset: offset + 'px',
+				mobileHA: false,
+				container: scrollCtn
+			});
+		}
+		
+		t.sendClickEvent();
+		
+		return window.pd(e);
+	};
+	
+	var init = function () {
+		site.on($.click, '.js-scroll-to-id-button' , scrollToIdClicked);
+	};
+	
+	App.modules.exports('auto-scroll-to-id', {
+		init: init
+	});
+	
+})(jQuery);
