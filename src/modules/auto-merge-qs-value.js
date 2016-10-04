cce110a2f4441be4a5df310465657f72566c1223
Pascal Piché <pascal@nitriques.com>
2016-10-03 20:39:19 -0400

    New module: Auto merge qs value (#25)
    * Added new module : auto-merge-qs-value
    * Removing unnecessary function. Fix 'click' handle to use $.click instead

---
+++ src/modules/auto-merge-qs-value.js
@@ -0,0 +1,57 @@
+/**
+ * @author Deux Huit Huit
+ * 
+ *
+ */
+(function ($, undefined) {
+	
+	'use strict';
+	var win = $(window);
+	var site = $('#site');
+	var curPage = $();
+	
+	var BUTTON_SELECTOR = '.js-merge-qs-value-button';
+	var KEY_ATTR = 'data-merge-qs-value-key';
+	var VALUE_ATTR = 'data-merge-qs-value';
+
+	
+	var buttonClicked = function (e) {
+		
+		//Scroll To hash
+		var t = $(this);
+		var key = t.attr(KEY_ATTR);
+		var value = t.attr(VALUE_ATTR);
+		var qs = window.QueryStringParser.parse(document.location.search);
+		
+		//Minimal attribute needed for proceeding
+		if(key) {
+
+			//Build new qs
+			if (value) {
+				qs[key] = value;
+			} else {
+				qs[key] = null;
+			}
+
+			//Update Url and raise fragmentChanged
+			App.mediator.notify('page.updateQsFragment', {qs: qs, raiseFragmentChanged: true});
+
+			return window.pd(e, true);
+		}
+	};
+
+	
+	var init = function () {
+		site.on($.click, BUTTON_SELECTOR, buttonClicked);
+	};
+	
+	var actions = function () {
+		return { };
+	};
+	
+	App.modules.exports('auto-merge-qs-value', {
+		init: init,
+		actions: actions
+	});
+	
+})(jQuery);
