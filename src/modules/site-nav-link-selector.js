f56d7b93888b85e2a105fed9feab9287456776fd
Pascal Piché <pascal@nitriques.com>
2016-10-03 20:35:53 -0400

    Update: Site nav link selector with class attribute (#23)
    * New version with Attr to edit selected class from the html
    * Scope module inside #site

--- src/modules/site-nav-link-selector.js
+++ src/modules/site-nav-link-selector.js
@@ -5,24 +5,42 @@
 (function ($, undefined) {
 	
 	'use strict';
-	var win = $(window);
+
+	var BTN_SELECTOR = '.js-site-nav-link';
+	var SELECTED_CLASS = 'is-selected';
+	var ATTR_SELECTED_ADD = 'data-selected-class-add';
+	var ATTR_SELECTED_REMOVE = 'data-selected-class-remove';
 	var site = $('#site');
-	var siteNav = $('.js-site-nav');
 	
 	var updateSelectedLink = function (newKey) {
-		var newBtn = $('.js-nav-link-button.btn-' + newKey, siteNav);
-		$('.js-nav-link-button', siteNav).removeClass('is-selected');
-		newBtn.addClass('is-selected');
+		var newBtn = site.find(BTN_SELECTOR + '.btn-' + newKey);
+
+		//Remove Selected state on all node
+		site.find(BTN_SELECTOR + '.' + SELECTED_CLASS)
+			.not(newBtn)
+			.each(function () {
+
+				//Remove is selected state
+				var t = $(this);
+				t.removeClass(SELECTED_CLASS)
+					.removeClass(t.attr(ATTR_SELECTED_ADD))
+					.addClass(t.attr(ATTR_SELECTED_REMOVE));
+			});
+
+		//Add class
+		newBtn.each(function () {
+			var t = $(this);
+
+			t.addClass(SELECTED_CLASS)
+				.addClass(t.attr(ATTR_SELECTED_ADD))
+				.removeClass(t.attr(ATTR_SELECTED_REMOVE));
+		});
 	};
 	
 	var onEntering = function (key, data) {
 		//check page
 		var newPage = $(data.page.key());
 		updateSelectedLink(data.page.key().substring(1));
-	};
-		
-	var init = function () {
-		
 	};
 	
 	var actions = function () {
@@ -33,8 +51,7 @@
 		};
 	};
 	
-	var SiteHeader = App.modules.exports('site-header-link-selector', {
-		init: init,
+	App.modules.exports('site-nav-link-selector', {
 		actions: actions
 	});
 	
