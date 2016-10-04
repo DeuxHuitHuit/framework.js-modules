3b81abe335ff8e6fb3b3f2665c4c0f6dd0d3fa08
nitriques <nico@nitriques.com>
2016-10-03 20:56:05 -0400

    whitespaces fix

--- src/modules/auto-flickity.js
+++ src/modules/auto-flickity.js
@@ -4,39 +4,39 @@
  * Auto flickity module
  */
 (function ($, undefined) {
-
+	
 	'use strict';
 	
 	var win = $(window);
 	var site = $('#site');
 	var page = $('.page');
-
+	
 	/*
 		Front-end Integration Hierarchy:
-
+		
 		|- FLICKITY CTN : .js-auto-flickity-slider-ctn
 		|		|- CELL-CTN : .js-auto-flickity-ctn
 		|		|		|- CELL (REPEATED): .js-auto-flickity-item
 		|		|		|
 		|		|- NAV BTNS: .js-auto-flickity-nav-btn
-
-
+		
+		
 		Notes:
-
+		
 		Flickity wont be activated if only one cell(o.cellSelector) is
 		detected. It will add the aborted class(o.abortedCl) on the cell-ctn(o.cellCtn)
-
+		
 		You can have more info on the options of Flickity at
 		http://flickity.metafizzy.co/
-
-	 */
-
+		
+	*/
+	
 	var o = {
 		sliderCtn: '.js-auto-flickity-slider-ctn',
 		cellCtn:'.js-auto-flickity-ctn',
 		cellSelector: '.js-auto-flickity-item',
 		navBtnSelector:'.js-auto-flickity-nav-btn',
-
+		
 		abortedClass:'is-flickity-cancelled',
 		initedClass:'is-flickity-inited',
 		selectedClass:'is-selected',
@@ -46,7 +46,7 @@
 		cellAlign: 'left',
 		wrapAround: true
 	};
-
+	
 	var onResize = function () {
 		$(o.cellCtn + '.' + o.initedClass).each(function () {
 			$(this).flickity('resize');
@@ -55,11 +55,10 @@
 	
 	var pageEnter = function (key, data) {
 		page = $(data.page.key());
-
+		
 		page.find(o.cellCtn + ':not('+ o.initedClass +')').each(function() {
-
 			var t = $(this);
-
+			
 			if (t.find(o.cellSelector).length > 1) {
 				t.flickity(o).addClass(o.initedClass);
 				onResize();
@@ -71,9 +70,8 @@
 				});
 			}
 		});
-
 	};
-
+	
 	var pageLeave = function () {
 		page.find(o.initedClass).each(function(){
 			$(this).flickity('destroy').removeClass(o.initedClass);
@@ -81,17 +79,16 @@
 		
 		page = $();
 	};
-
+	
 	var onNavBtnClick = function (e) {
-
 		var t = $(this);
 		var slider = t.closest(o.sliderCtn).find(o.cellCtn);
 		var action = t.attr('data-auto-flickity-action');
-
+		
 		if (!!action) {
 			slider.flickity(action, true);
 		}
-
+		
 		return window.pd(e);
 	};
 	
@@ -110,8 +107,8 @@
 			}
 		};
 	};
-
-	var AutoFlickity = App.modules.exports('auto-flickity',  { 
+	
+	var AutoFlickity = App.modules.exports('auto-flickity', {
 		init: init,
 		actions: actions
 	});
