53a5b1b8a64a278006d5e16fa4c0f7003cda5346
Nicolas Brassard <nico@nitriques.com>
2016-12-09 17:12:43 -0500

    Manually cancels clicks in links modules

--- src/modules/links.js
+++ src/modules/links.js
@@ -18,37 +18,47 @@
 	var origin = loc.origin || (loc.protocol + '//' + loc.hostname);
 	var originRegExp = new RegExp('^' + origin, 'i');
 	
-	var onClickGoto = function (e) {
-		var t = $(this);
-		var href = t.attr('href');
-		
+	var mustIgnore = function (t, e) {
 		// ignore click since there are no current page
 		if (!App.mediator._currentPage()) {
 			return true;
 		}
 		
 		// ignore click since it's not http
+		var href = t.attr('href');
 		if (/^(mailto|skype|tel|ftps?|#)/im.test(href)) {
 			return true;
 		}
 		
 		// no keys on the keyboard
-		if (!e.metaKey && !e.ctrlKey) {
-			if (/^\?.+/.test(href)) {
-				href = window.location.pathname + href;
-			}
-			if (originRegExp.test(href)) {
-				href = href.replace(originRegExp, '');
-			}
-			
-			App.mediator.notify('links.gotoClicked', {
-				item: t, url: href
-			});
-			
-			App.mediator.goto(href);
-			
-			return window.pd(e);
+		if (!!e.metaKey || !!e.ctrlKey) {
+			return true;
 		}
+		return false;
+	}
+	
+	var onClickGoto = function (e) {
+		var t = $(this);
+		var href = t.attr('href');
+		
+		if (mustIgnore(t, e)) {
+			return true;
+		}
+		
+		if (/^\?.+/.test(href)) {
+			href = window.location.pathname + href;
+		}
+		if (originRegExp.test(href)) {
+			href = href.replace(originRegExp, '');
+		}
+		
+		App.mediator.notify('links.gotoClicked', {
+			item: t, url: href
+		});
+		
+		App.mediator.goto(href);
+		
+		return window.pd(e);
 	};
 	
 	var onClickToggle = function (e) {
@@ -56,7 +66,7 @@
 		var href = t.attr('href');
 		var fallback = t.attr('data-toggle-fallback-url');
 		
-		if (!App.mediator._currentPage()) {
+		if (mustIgnore(t, e)) {
 			return true;
 		}
 		
@@ -71,8 +81,12 @@
 		return window.pd(e);
 	};
 	
-	var actions = function () {
-		return {};
+	var onClickCancel = function (e) {
+		var t = $(this);
+		if (mustIgnore(t, e)) {
+			return true;
+		}
+		return window.pd(e);
 	};
 	
 	var init = function () {
@@ -85,20 +99,25 @@
 		var toggleLinks = '[data-action="toggle"]';
 		var absoluteLinks = 'a[href^="/"]';
 		var queryStringLinks = 'a[href^="?"]';
-		var click = App.device.events.pick;
+		var pick = App.device.events.pick;
+		var click = App.device.events.click;
 
 		// capture all click in #site
 		$('#site')
-			.on(click, absoluteLinks + workspaceExclusion + dataAttrExclusions, onClickGoto)
-			.on(click, queryStringLinks + workspaceExclusion + dataAttrExclusions, onClickGoto)
-			.on(click, localLinks + dataAttrExclusions + localWorkspaceExclusion, onClickGoto)
-			.on(click, absoluteLinks + toggleLinks, onClickToggle)
-			.on(click, queryStringLinks + toggleLinks, onClickToggle);
+			.on(pick, absoluteLinks + workspaceExclusion + dataAttrExclusions, onClickGoto)
+			.on(pick, queryStringLinks + workspaceExclusion + dataAttrExclusions, onClickGoto)
+			.on(pick, localLinks + dataAttrExclusions + localWorkspaceExclusion, onClickGoto)
+			.on(pick, absoluteLinks + toggleLinks, onClickToggle)
+			.on(pick, queryStringLinks + toggleLinks, onClickToggle)
+			.on(click, absoluteLinks + workspaceExclusion + dataAttrExclusions, onClickCancel)
+			.on(click, queryStringLinks + workspaceExclusion + dataAttrExclusions, onClickCancel)
+			.on(click, localLinks + dataAttrExclusions + localWorkspaceExclusion, onClickCancel)
+			.on(click, absoluteLinks + toggleLinks, onClickCancel)
+			.on(click, queryStringLinks + toggleLinks, onClickCancel)
 	};
 	
 	var Links = App.modules.exports('links', {
-		init: init,
-		actions: actions
+		init: init
 	});
 	
 })(jQuery);
