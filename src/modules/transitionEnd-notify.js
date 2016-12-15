95b77effdbdf6ee710e7807f3723e528dc842120
Pascal Piché <pascal@nitriques.com>
2016-12-15 14:37:31 -0500

    Added cancel action (#64)

--- src/modules/transitionEnd-notify.js
+++ src/modules/transitionEnd-notify.js
@@ -7,24 +7,28 @@
 	'use strict';
 	var win = $(window);
 
-	var onAttach = function (key, data) {
+	var onCancel = function (key, data) {
+		if (data && data.item) {
+			$.removeFromTransition(data.item.selector);
+		}
+	};
 
+	var onAttach = function (key, data) {
 		if (data && data.item) {
 			var notifyEnd = data.item.attr('data-transitionEnd-notify');
 			if (notifyEnd && notifyEnd.length) {
-
 				data.item.transitionEnd(function () {
 					App.mediator.notify(notifyEnd, {item: data.item});
-				})
+				});
 			}
-			
 		}
 	}
 	
 	var actions = function () {
 		return {
 			transitionEndNotify: {
-				attach: onAttach
+				attach: onAttach,
+				cancel: onCancel
 			}
 		};
 	};
