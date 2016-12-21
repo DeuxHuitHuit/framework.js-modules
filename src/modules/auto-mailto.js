7853817f771c8f92b3ac6dca585f306591cb3613
beaubbe <beaubbe@gmail.com>
2016-12-21 13:56:04 -0500

    Added auto-mailto module (#69)
    * Added auto-mailto module 4a3fe19
    
    This module will inject the href of mailto links to prevent cloudflare from messing with the mailto url
    
    * Moved mailto: to module auto-mailto f2ccc5f
    
    Re : https://github.com/DeuxHuitHuit/symphony-2-template/pull/125

---
+++ src/modules/auto-mailto.js
@@ -0,0 +1,50 @@
+/**
+ * @author Deux Huit Huit
+ * 
+ * auto jit image
+ */
+(function ($, global, undefined) {
+	
+	'use strict';
+	
+	var firstTime = true;
+	var site = $('#site');
+	var page = $('.page');
+	
+	var update = function (ctn) {
+		ctn.find('a[data-mailto]').each(function () {
+			var t = $(this);
+			t.attr('href', 'mailto:' +  t.attr('data-mailto'));
+		});
+	}
+
+	var onArticleEnter = function (key, data) {
+		update(data.article);
+	};
+
+	var onEnter = function (key, data) {
+		page = $(data.page.key());
+		update(page);
+	};
+
+	var init = function () {
+		update(site);
+	};
+	
+	var actions = function () {
+		return {
+			page: {
+				enter: onEnter
+			},
+			articleChanger: {
+				enter: onArticleEnter
+			}
+		};
+	};
+	
+	var AutoJitImage = App.modules.exports('auto-mailto', {
+		init: init,
+		actions: actions
+	});
+	
+})(jQuery, window);
