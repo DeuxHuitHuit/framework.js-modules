33be4521fe1d493fa79e137a83d2049f07d958e6
Pascal Piché <PascalPiche@users.noreply.github.com>
2017-08-30 17:38:55 -0400

    Refactor: Fix jshint and complexity (#109)

--- src/modules/cloudflare-email.js
+++ src/modules/cloudflare-email.js
@@ -17,41 +17,52 @@
 	var SELECTOR = 'a[href^="/cdn-cgi/l/email-protection"]';
 	var PATTERN = /^\[email([\u0080-\u00FF ]+)protected\]$/i;
 
+	var process = function (a, e) {
+		var j,c,s = '';
+
+		var r = parseInt(a.substr(0, 2), 16);
+
+		if (r) {
+			for (j = 2; j < a.length; j += 2) {
+				c = parseInt(a.substr(j, 2), 16) ^ r;
+				s += String.fromCharCode(c);
+			}
+
+			e.attr('href', 'mailto:' + s);
+			e.find('script').remove();
+			
+			var span = e.find('.__cf_email__');
+
+			if (!!span.length) {
+				e = span;
+			}
+
+			if (PATTERN.test(str.trim(e.text()))) {
+				e.text(s);
+			}
+		}
+	};
+
 	var doIt = function () {
 		$(SELECTOR).each(function (i, e) {
-			/* jshint ignore:start */
 			try {
 				e = $(e);
 				var a = e.attr('data-cfemail');
+
 				if (!a) {
 					a = e.attr('href').split('#')[1];
 				}
+
 				if (a && a.indexOf('?') !== -1) {
 					a = a.split('?')[0];
 				}
+
 				if (a) {
-					var j,c,s = '';
-					var r = parseInt(a.substr(0, 2), 16);
-					if (r) {
-						for (j = 2; j < a.length; j += 2) {
-							c = parseInt(a.substr(j, 2), 16) ^ r;
-							s += String.fromCharCode(c);
-						}
-						e.attr('href', 'mailto:' + s);
-						e.find('script').remove();
-						var span = e.find('.__cf_email__');
-						if (!!span.length) {
-							e = span;
-						}
-						if (PATTERN.test(str.trim(e.text()))) {
-							e.text(s);
-						}
-					}
+					process(a, e);
 				}
 			} catch (ex) {
 				App.log(ex);
 			}
-			/* jshint ignore:end */
 		});
 	};
 
