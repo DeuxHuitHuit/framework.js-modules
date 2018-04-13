e8c6d50c01726dacb7ba82168dcd13e46e3df098
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-04-13 17:02:25 -0400

    Add keynav support for popups (#142)
    - Disables all focusable elements not in the popup, by setting their
    tabindex to -1
    - The tabindex is reverted to the original value when the popup closes
    - The ESCAPE key is used to close the popup: it clicks on the close
    button.
    
    Picked from 54ffb6a

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -11,6 +11,72 @@
 	var POPUP_SELECTOR = '.js-popup';
 	var BG_SELECTOR = '.js-popup-bg';
 	var RESET_ON_CLOSE_ATTR = 'data-popup-reset-on-close';
+	var FOCUSABLE = [
+		'a[href]',
+		'area[href]',
+		'input:not([disabled])',
+		'select:not([disabled])',
+		'textarea:not([disabled])',
+		'button:not([disabled])',
+		'iframe, object, embed',
+		'[tabindex], [contenteditable]'
+	].join(',');
+	
+	var focusable = function (popup) {
+		return $(FOCUSABLE).filter(function () {
+			var t = $(this);
+			// element not in popup or is background
+			return !t.closest(popup).length || t.is(BG_SELECTOR);
+		});
+	};
+	
+	var restoreFocusable = function (i, e) {
+		var t = $(e);
+		var tab = t.data('acc-tabindex');
+		if (tab) {
+			t.attr('tabindex', tab);
+		} else {
+			t.removeAttr('tabindex');
+		}
+	};
+	
+	var keyUp = function (e) {
+		if (e.which === window.keys.escape) {
+			$(this).find('.js-popup-close-btn').click();
+		}
+	};
+	
+	var removeFocusable = function (i, e) {
+		var t = $(e);
+		var tab = t.attr('tabindex');
+		if (tab === '-1') {
+			return;
+		}
+		t.data('acc-tabindex', tab || undefined);
+		t.attr('tabindex', '-1');
+	};
+	
+	var a11y = function (popup) {
+		if (!popup || !popup.length) {
+			return;
+		}
+		// Disable focus for accessibility
+		focusable(popup).each(removeFocusable);
+		// Re enable focus for accessibility
+		popup.find(FOCUSABLE).each(restoreFocusable);
+		popup.on('keyup', keyUp);
+	};
+	
+	var a11yReset = function (popup) {
+		if (!popup || !popup.length) {
+			return;
+		}
+		// Re enable focus for accessibility
+		focusable(popup).each(restoreFocusable);
+		// Disable focus for accessibility
+		popup.find(FOCUSABLE).each(restoreFocusable);
+		popup.off('keyup', keyUp);
+	};
 	
 	var toggleAnimInited = function (action, popup) {
 		popup.addClass('noanim');
@@ -51,6 +117,7 @@
 			});
 			// do the anim
 			toggleAnim('on', popup);
+			a11y(popup);
 		}
 	};
 	
@@ -77,6 +144,7 @@
 			
 			//do the anim
 			toggleAnim('off', popup);
+			a11yReset(popup);
 		}
 	};
 	
@@ -91,6 +159,10 @@
 		}
 	};
 	
+	var init = function () {
+		a11y($(POPUP_SELECTOR));
+	};
+	
 	var actions = function () {
 		return {
 			popup: {
@@ -102,7 +174,8 @@
 	};
 	
 	App.modules.exports('popup', {
-		actions: actions
+		actions: actions,
+		init: init
 	});
 	
 })(jQuery);
