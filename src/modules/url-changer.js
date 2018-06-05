DeuxHuitHuit/framework.js-modules

commit 44abcd9ce960cce52311b2d46d30196e0484f6f3

Move pageview call into page.enter
This fixes a problem where document.title would not be up to date

src/modules/url-changer.js
@@ -142,8 +142,6 @@
 			currentPageRoute = nextRoute;
 			currentPageUrl = data.route;
 			currentPageKey = data.page.key();
-			
-			$.sendPageView({page: data.route});
 		}
 	};
 	
@@ -155,6 +153,8 @@
 				window.location.host + currentPageRoute;
 			currentPageFragment = href.substring(curPageHref.length);
 			App.mediator.notify('page.fragmentChanged', currentPageFragment);
+		} else {
+			$.sendPageView({page: data.route});
 		}
 	};
 	

commit 409c0559697ce79121de95bf364b72ec383ade35

Check if page.fragmentChanged was handled or not (#157)
* Fix weird if statement b8c1dc4

A else works here

* Trigger fragment change only if route needs it  5275c63

If not, we are not able to track page view properly.
This is a regression from 44abcd9ce960cce52311b2d46d30196e0484f6f3

* Check if page.fragmentChanged was handled or not  b6a8c2d

Instead of the route instead of checking the route itself like 5275c63209 did.

* Rewrite the generateQsString() with arrays 7445f52

+ call window.encodeURIComponent() for safety

src/modules/url-changer.js
@@ -120,10 +120,9 @@
 		
 		if (currentPageRoute === '') {
 			triggerFirstFragmentChange = true;
-		}
-		if (currentPageRoute !== '') {
+		} else {
 			url = url + currentPageFragment;
-		
+
 			if (!isPopingState) {
 				historyPush(newRoute + currentPageFragment);
 			}
@@ -146,14 +145,19 @@
 	};
 	
 	var onPageEntered = function (key, data, e) {
+		var handled = false;
 		if (triggerFirstFragmentChange) {
-			//Detect if we have a fragment
+			// Detect if we have a fragment
 			var href = window.location.href;
 			var curPageHref = window.location.protocol + '//' +
 				window.location.host + currentPageRoute;
 			currentPageFragment = href.substring(curPageHref.length);
-			App.mediator.notify('page.fragmentChanged', currentPageFragment);
-		} else {
+			App.mediator.notify('page.fragmentChanged', currentPageFragment, function () {
+				handled = true;
+			});
+		}
+		
+		if (!handled) {
 			$.sendPageView({page: data.route});
 		}
 	};
@@ -180,18 +184,18 @@
 	};
 	
 	var generateQsString = function () {
-		var result = '';
+		var result = [];
 		
 		$.each(currentQsFragment, function (prop, value) {
+			prop = window.encodeURIComponent(prop);
 			if (!!value) {
-				if (!!result.length) {
-					result += '&';
-				}
-				result += (prop + '=' + value);
+				result.push(prop + '=' + window.encodeURIComponent(value));
+			} else {
+				result.push(prop);
 			}
 		});
 		
-		return result;
+		return result.join('&');
 	};
 	
 	var onUpdateQsFragment = function (key, options, e) {