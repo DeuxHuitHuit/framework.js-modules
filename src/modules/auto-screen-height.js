1d2da06bdcc203a1b05e09014be0c8d2ab3d423b
Pascal Piché <PascalPiche@users.noreply.github.com>
2018-11-01 16:09:58 -0400

    Auto screen height add max width, scope to entire site not page (#186)
    * Add empty line 394d078fc409144fefe46e080f27bbb210881eb0
    * Scope auto-screen-height to all element in the site c02da04d97019e2198f9a1f1449c9b95d32485c8
    * Add maxWidth and better mediaQuery algo (fallback if not supported) f5a684943da1d950ec031669c2a3ba0706b49543

--- src/modules/auto-screen-height.js
+++ src/modules/auto-screen-height.js
@@ -6,15 +6,12 @@
 (function ($, undefined) {
 
 	'use strict';
-	
+
 	var win = $(window);
-	
+	var site = $('#site');
+
 	var mobileHeight = 0;
-	
-	var getPage = function () {
-		return $('.page:visible');
-	};
-	
+
 	var platforms = {
 		all: true,
 		desktop: !App.device.mobile,
@@ -22,10 +19,10 @@
 		mobile: App.device.mobile,
 		phone: $.phone
 	};
-	
+
 	var getOffsetTotal = function (itemsArray) {
 		var total = 0;
-		
+
 		if (itemsArray) {
 			var its = itemsArray.split(',');
 			$.each(its, function (i, value) {
@@ -34,12 +31,12 @@
 		}
 		return total;
 	};
-	
+
 	var processPlatforms = function (itemsArray) {
 		var result = false;
-		
+
 		if (itemsArray) {
-		
+
 			var its = itemsArray.split(',');
 			$.each(its, function (i, value) {
 				if (platforms[value]) {
@@ -49,65 +46,92 @@
 		}
 		return result;
 	};
-	
+
 	var resizeItem = function () {
 		var t = $(this);
+		var winWidth = win.width();
 		var ratio = t.attr('data-height-ratio') || 1;
 		var fx = t.attr('data-height-property') || 'minHeight';
 		var offset = getOffsetTotal(t.attr('data-height-offset'));
 		var newHeight = (win.height() - offset) * ratio;
 		var platformsVal = processPlatforms(t.attr('data-screen-height-platform') || 'all');
-		var minWidth = t.attr('data-screen-height-min-width') || 0;
-		var useMediaQuery = t.data('data-screen-height-use-media-query') || true;
+		var minWidth = parseInt(t.attr('data-screen-height-min-width'), 10) || 0;
+		var maxWidth = parseInt(t.attr('data-screen-height-max-width'), 10) || 0;
+		var useMediaQuery = t.attr('data-screen-height-use-media-query');
 		var useJitImage = t.attr('data-screen-height-jitimage') || true;
-		
+		var makeMediaQuery = function makeMediaQuery () {
+			var result = '';
+
+			var hasMinWidth = minWidth !== 0;
+			var hasMaxWidth = maxWidth !== 0;
+
+			if (hasMinWidth) {
+				result = '(min-width: ' + minWidth + 'px)';
+			}
+			if (hasMinWidth && hasMaxWidth) {
+				result += ' and ';
+			}
+			if (hasMaxWidth) {
+				result += '(max-width: ' + maxWidth + 'px)';
+			}
+			return result;
+		};
+		var mediaQuery = makeMediaQuery();
+		var boolUseMediaQuery = true;	//True by default
+		if (useMediaQuery && useMediaQuery.length) {
+			boolUseMediaQuery = useMediaQuery === 'true';
+		}
+
+		//Disable Media Query when not supported
+		boolUseMediaQuery = !!window.matchMedia && boolUseMediaQuery;
+
 		//test platforms
 		if (platformsVal &&
-			!useMediaQuery &&
-			win.width() > minWidth) {
-				
+			!boolUseMediaQuery &&
+			winWidth > minWidth &&
+			(maxWidth === 0 || winWidth < maxWidth)) {
+
 			t.css(fx, newHeight);
-			
 		} else if (platformsVal &&
-			useMediaQuery &&
-			window.matchMedia('(min-width: ' + minWidth + 'px)').matches) {
+			boolUseMediaQuery &&
+			(mediaQuery.length === 0 || window.matchMedia(mediaQuery).matches)) {
+
 			t.css(fx, newHeight);
 		} else {
 			t.css(fx, '');
 		}
+
 		if (useJitImage) {
 			$('img[data-src-format]', t).jitImage();
 		}
 	};
-	
+
 	var onResize = function (e) {
-		var p = getPage();
 		if ((App.device.mobile && Math.abs(mobileHeight - win.height()) > 120) ||
 			!App.device.mobile) {
-			p.filter('.js-auto-screen-height')
-				.add($('.js-auto-screen-height', p))
+			site.filter('.js-auto-screen-height')
+				.add($('.js-auto-screen-height', site))
 				.each(resizeItem);
 			mobileHeight = win.height();
 		}
 	};
-	
+
 	var onEnter = function () {
 		mobileHeight = 0;
 		onResize();
 		if (App.device.mobile) {
 			mobileHeight = win.height();
 		}
-		
 		setTimeout(onResize, 100);
 	};
-	
+
 	var init = function () {
 		onResize();
 		if (App.device.mobile) {
 			mobileHeight = win.height();
 		}
 	};
-	
+
 	var actions = function () {
 		return {
 			site: {
@@ -129,5 +153,5 @@
 		init: init,
 		actions: actions
 	});
-	
+
 })(jQuery);
