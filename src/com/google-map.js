3792adb4c32ca5522de96ff2bf2be3a3f287c0e3
Nicolas Brassard <nitriques@users.noreply.github.com>
2019-04-24 20:51:36 -0400

    Google-map update (#205)
    * Add more callbacks and public API
    
    - Add drag and boundsi change callback
    - Add geocoder api (requires the Geocoder API to be allowed with the
    key)
    
    * Make the component load its own deps

--- src/com/google-map.js
+++ src/com/google-map.js
@@ -2,17 +2,26 @@
  * @author Deux Huit Huit
  *
  * Google maps component
+ *
+ * It needs to have the API key in the DOM as
+ * <script data-gmap-key="<YOUR KEY>"></script>
  */
 (function ($, win, global, undefined) {
 
 	'use strict';
 	
 	App.components.exports('googleMap', function (o) {
-		
+		var MAX_RETRIES = 40;
+		var scope;
 		var container;
 		var markers = [];
 		var map;
+		var mapIsReady = false;
 		var openedMarker;
+		var urlGoogleMaps = 'https://maps.googleapis.com/maps/api/js?key=' +
+			$('[data-gmap-key]').attr('data-gmap-key');
+		var urlInfobox = 'https://cdn.jsdelivr.net/gh/googlemaps/v3-utility-library@' +
+			'4cd67683a905265bfe5108205d8fff676eced4d8/infobox/src/infobox_packed.js';
 		
 		var closeAllPopup = function () {
 			if (!!openedMarker) {
@@ -21,8 +30,10 @@
 		};
 		
 		var defaultMapOptions = {
+			selectorCtn: '',
 			defaultMarkerOptions: {},
 			mapTypeId: null,
+			zoom: 10,
 			markerAction: function () {
 				var reelPosition = new google.maps.LatLng(
 					this.getPosition().lat() + 0.005,
@@ -34,10 +45,35 @@
 				openedMarker = this;
 			},
 			beforeCreate: null,
-			afterCreate: null
+			afterCreate: null,
+			boundsChanged: null,
+			dragBoundsChanged: null,
+			dragBoundsChangedDelay: 250,
+			drag: null,
+			styles: []
 		};
 		
 		var mapOptions = $.extend({}, defaultMapOptions, o);
+		
+		var googleGeocoder = function () {
+			return !!global.google && !!google.maps && !!google.maps.Geocoder;
+		};
+		
+		// GEO LOCALISATION
+		var geolocAddress = function (data, callback) {
+			App.loaded(googleGeocoder, function () {
+				var geo = new google.maps.Geocoder();
+				var res;
+
+				geo.geocode(data, function (results, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						res = results[0].geometry.location;
+					}
+					
+					App.callback(callback, [res, results]);
+				});
+			}, MAX_RETRIES);
+		};
 		
 		var addMarker = function (o) {
 			
@@ -66,13 +102,15 @@
 					markerOption.position.longitude
 				);
 			}
-			
+
 			var marker = new google.maps.Marker({
 				position: markerOption.LatLng,
 				map: map,
 				icon: markerOption.iconImage,
 				shadow: markerOption.iconShadow,
-				zIndex: markerOption.zIndex
+				zIndex: markerOption.zIndex,
+				animation: markerOption.animation,
+				optimized: false
 			});
 			
 			markers.push(marker);
@@ -84,9 +122,17 @@
 				});
 				
 				google.maps.event.addListener(marker, 'click', mapOptions.markerAction);
+			} else if (markerOption.markerCustomAction) {
+				google.maps.event.addListener(marker, 'click', function () {
+					App.callback(markerOption.markerCustomAction, [map, marker]);
+				});
 			} else if (mapOptions.markerCustomAction) {
-				google.maps.event.addListener(marker, 'click', mapOptions.markerCustomAction);
+				google.maps.event.addListener(marker, 'click', function () {
+					App.callback(mapOptions.markerCustomAction, [map, marker]);
+				});
 			}
+			
+			return marker;
 		};
 		
 		var createMap = function () {
@@ -106,9 +152,28 @@
 			}
 			map = new google.maps.Map(container.get(0), mapOptions);
 			
-			google.maps.event.addListener(map, 'bounds_changed', function () {
-				//notify page that bounds changed
-				App.mediator.notifyCurrentPage('map.boundsChanged', map.getBounds());
+			if (!!mapOptions.boundsChanged) {
+				google.maps.event.addListener(map, 'bounds_changed', function () {
+					var bounds = map.getBounds();
+					App.callback(mapOptions.boundsChanged, [bounds]);
+				});
+			}
+			if (!!mapOptions.dragBoundsChanged) {
+				var debouncedDragEnd = _.debounce(function () {
+					var bounds = map.getBounds();
+					App.callback(mapOptions.dragBoundsChanged, [bounds]);
+				}, mapOptions.dragBoundsChangedDelay);
+				google.maps.event.addListener(map, 'dragend', debouncedDragEnd);
+			}
+			if (!!mapOptions.drag) {
+				google.maps.event.addListener(map, 'drag', function () {
+					var bounds = map.getBounds();
+					App.callback(mapOptions.drag, [bounds]);
+				});
+			}
+			google.maps.event.addListenerOnce(map, 'idle', function () {
+				mapIsReady = true;
+				App.callback(mapOptions.ready, [google.maps]);
 			});
 			
 			App.callback(mapOptions.afterCreate, [google.maps]);
@@ -125,28 +190,82 @@
 			}
 		};
 		
-		var init = function (p, selector) {
-			container = $(selector, p);
-			App.loaded(googleMap, function () {
-				initMap();
-			});
+		var loadDeps = function (ready) {
+			ready = ready || $.noop;
+			if (!window.isMapApiLoaded) {
+				$.getScript(urlGoogleMaps).then(function () {
+					$.getScript(urlInfobox).then(ready);
+				});
+				window.isMapApiLoaded = true;
+			} else {
+				ready();
+			}
+		};
+		
+		var init = function (s, options, initCompleteCallback) {
+			mapOptions = $.extend(mapOptions, options);
+			scope = s;
+			container = scope.find(mapOptions.selectorCtn);
+			var ready = function () {
+				App.loaded(googleMap, function () {
+					initMap();
+					App.callback(initCompleteCallback);
+				}, MAX_RETRIES);
+			};
+			
+			loadDeps(ready);
 		};
 		
 		return {
 			init: init,
+			loadDeps: loadDeps,
 			addMarker: addMarker,
 			center: function (lat, lng) {
 				map.panTo(new google.maps.LatLng(lat, lng));
+				App.log('[map] center ' + lat + ', ' + lng);
 			},
 			zoom: function (value) {
 				map.setZoom(value);
+				App.log('[map] zoom ' + value);
 			},
-			fitBounds: function (viewport) {
+			fitBounds: function (viewport, padding) {
 				map.fitBounds(new google.maps.LatLngBounds(
 					viewport.southwest, viewport.northeast
-				));
+				), padding);
+				App.log('[map] fitBound ' +
+					viewport.southwest.lng() + ', ' +
+					viewport.southwest.lat() + ' - ' +
+					viewport.northeast.lng() + ', ' +
+					viewport.northeast.lat()
+				);
 			},
-			closeAllPopup: closeAllPopup
+			bounds: function () {
+				return map.getBounds();
+			},
+			decZoom: function () {
+				map.setZoom(Math.min(1, map.getZoom() - 1));
+				App.log('[map] decZoom');
+			},
+			closeAllPopup: closeAllPopup,
+			setMarkerVisibility: function (marker, isVisible) {
+				marker.setVisible(isVisible);
+			},
+			getMarkers: function () {
+				return markers;
+			},
+			geolocAddress: geolocAddress,
+			googleMap: function () {
+				return googleMap();
+			},
+			mapIsReady: function () {
+				return googleMap() && !!map && !!mapIsReady;
+			},
+			addListenerOnce: function (event, fn) {
+				return google.maps.event.addListenerOnce(map, event, fn);
+			},
+			getZoom: function () {
+				return map.getZoom();
+			}
 		};
 	});
 	
