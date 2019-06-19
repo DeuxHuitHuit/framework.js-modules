d271d61a298590d25e6f6bf3f9c06692f769d66a
dominic-blain <dominic.blain@gmail.com>
2019-06-19 17:02:53 -0400

    (feat) Replace popup by modal (now auto-modal) (#239)
    * Replace popup by modal (now auto-modal) 76e124103fa18f915bb2efd42feda0c73b52d698
    * Normalise naming of module and file name 589ced18ab90596b8b0a531060819dc5759fb4ff
    * Create functions for hide and show background cc65c85e4186b0fee860fb5c05a63dfd81ff3a21

--- src/modules/auto-modal.js
+++ src/modules/auto-modal.js
@@ -1,52 +1,184 @@
 /**
  *  @author Deux Huit Huit
  *
- *  Auto modal
+ *  auto-modal
  */
 (function ($, undefined) {
 	
 	'use strict';
-	var win = $(window);
-	var site = $('#site');
-	var BTN_SELECTOR = '.js-auto-modal-btn';
-	var COMMON_ANCESTOR_SELECTOR_ATTR = 'data-auto-modal-common-ancestor';
-	var POPUP_SELECTOR_ATTR = 'data-auto-modal-popup-selector';
-	var INNER_SCROLL_ATTR = 'data-inner-scroll-selector';
 	
-	var toggleModalState = function (popup, isPopep) {
-		var scrollFx = isPopep ? 'removeScroll' : 'addScroll';
-		App.modules.notify('site.' + scrollFx);
+	var site = $('#site');
+	var bgTransitionModal = $('#bg-transition-modal');
+	var FOCUSABLE = [
+		'a[href]',
+		'area[href]',
+		'input:not([disabled])',
+		'select:not([disabled])',
+		'textarea:not([disabled])',
+		'button:not([disabled])',
+		'iframe, object, embed',
+		'[tabindex], [contenteditable]'
+	].join(',');
+	var o = {
+		selectorToggle: '.js-modal-toggle'
 	};
-
-	var onButtonClick = function (e) {
-		var t = $(this);
-		var commonAncestor = t.closest(t.attr(COMMON_ANCESTOR_SELECTOR_ATTR));
-		var reelRef = !!commonAncestor.length ? commonAncestor : site;
-		var popup = reelRef.find(t.attr(POPUP_SELECTOR_ATTR));
-		
-		if (!!popup.length) {
-			App.modules.notify('popup.toggle', {
-				popup: popup,
-				openCallback: function () {
-					toggleModalState(popup, true);
-				},
-				closeCallback: function () {
-					popup.find(popup.attr(INNER_SCROLL_ATTR)).scrollTop(0);
-					toggleModalState(popup, false);
-				}
-			});
+	
+	var focusable = function (modal) {
+		return $(FOCUSABLE).filter(function () {
+			var t = $(this);
+			// element not in popup or is background
+			return !t.closest(modal).length || t.is(bgTransitionModal);
+		});
+	};
+	
+	var restoreFocusable = function (i, e) {
+		var t = $(e);
+		var tab = t.data('acc-tabindex');
+		if (tab) {
+			t.attr('tabindex', tab);
 		} else {
-			App.log('auto-modal: No popup found with selector : ' + t.attr(POPUP_SELECTOR_ATTR));
+			t.removeAttr('tabindex');
+		}
+	};
+	
+	var keyUp = function (e) {
+		if (e.which === window.keys.escape) {
+			$(this).find(o.selectorToggle).click();
+		}
+	};
+	
+	var removeFocusable = function (i, e) {
+		var t = $(e);
+		var tab = t.attr('tabindex');
+		if (tab === '-1') {
+			return;
+		}
+		t.data('acc-tabindex', tab || undefined);
+		t.attr('tabindex', '-1');
+	};
+	
+	var a11y = function (modal) {
+		if (!modal || !modal.length) {
+			return;
+		}
+		// Disable focus for accessibility
+		focusable(modal).each(removeFocusable);
+		// Re enable focus for accessibility
+		modal.find(FOCUSABLE).each(restoreFocusable);
+		$(document).on('keyup', keyUp);
+	};
+	
+	var a11yReset = function (modal) {
+		if (!modal || !modal.length) {
+			return;
+		}
+		// Re enable focus for accessibility
+		focusable(modal).each(restoreFocusable);
+		// Disable focus for accessibility
+		modal.find(FOCUSABLE).each(restoreFocusable);
+		$(document).off('keyup', keyUp);
+	};
+	
+	var toggleModal = function (item, isOn, callback) {
+		if (!!callback) {
+			$.removeFromTransition(item.selector);
+			item.transitionEnd(function () {
+				App.callback(callback);
+			}, item.selector);
 		}
 		
-		return window.pd(e);
+		App.modules.notify('changeState.update', {
+			item: item,
+			state: 'visible',
+			action: isOn ? 'on' : 'off'
+		});
+		
+		if (isOn) {
+			a11y(item);
+		} else {
+			a11yReset(item);
+		}
+	};
+	
+	var onPrepareModal = function (key, data) {
+		if (!!data && !!$(data.item).length) {
+			var item = $(data.item);
+			
+			item.addClass('noanim');
+			App.modules.notify('changeState.update', {
+				item: item,
+				state: 'visible',
+				action: 'off'
+			});
+			item.height();
+			item.removeClass('noanim');
+		}
+	};
+	
+	var onShowModal = function (key, data) {
+		if (!!data && !!$(data.item).length) {
+			var item = $(data.item);
+			onPrepareModal('', data);
+			toggleModal(item, true, data.callback);
+		}
+	};
+	
+	var onHideModal = function (key, data) {
+		if (!!data && !!$(data.item).length) {
+			var item = $(data.item);
+			toggleModal(item, false, data.callback);
+		}
+	};
+	
+	var toggleBg = function (isVisible, callback) {
+		if (!!callback) {
+			$.removeFromTransition(bgTransitionModal.selector);
+			bgTransitionModal.transitionEnd(function () {
+				App.callback(callback);
+			}, bgTransitionModal.selector);
+		}
+		App.modules.notify('changeState.update', {
+			item: bgTransitionModal,
+			state: 'visible',
+			action: isVisible ? 'on' : 'off'
+		});
+	};
+
+	var showBg = function (key, data) {
+		var callback = !!data ? data.callback : $.noop;
+		toggleBg(true, callback);
+	};
+
+	var hideBg = function (key, data) {
+		var callback = !!data ? data.callback : $.noop;
+		toggleBg(false, callback);
 	};
 
 	var init = function () {
-		site.on($.click, BTN_SELECTOR, onButtonClick);
+		// init a11y for popup that is already opened on load
+		// how to know which popup to do?
+		var page = $('.page');
+		var modal = page.find('.js-modal-page');
+		
+		if (modal.hasClass('is-visible')) {
+			a11y(modal);
+		}
+	};
+	
+	var actions = function () {
+		return {
+			modal: {
+				show: onShowModal,
+				hide: onHideModal,
+				prepare: onPrepareModal,
+				showBg: showBg,
+				hideBg: hideBg
+			}
+		};
 	};
 	
 	App.modules.exports('auto-modal', {
+		actions: actions,
 		init: init
 	});
 	
