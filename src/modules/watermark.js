f08805dea8843ef125eb34d4fa24314e7712f92d
Frederick Hamon <frederick@deuxhuithuit.com>
2020-07-06 17:08:55 -0400

    (fix) Fetch the watermark on site loaded after 3000ms (#402)
    Co-authored-by: Frederick Hamon <frederick@deuxhuithuit.co>

--- src/modules/watermark.js
+++ src/modules/watermark.js
@@ -12,24 +12,36 @@
 		ctn: '.js-watermark-ctn'
 	};
 
-	var init = function () {
-		site.find(sels.ctn).each(function () {
-			var t = $(this);
-			if (!!t.attr('data-href') && !t.html()) {
-				$.ajax({
-					method: 'GET',
-					crossDomain: true,
-					url: t.attr('data-href'),
-					success: function (data) {
-						t.append($(data).find('watermark').html());
-					}
-				});
+	var DELAY = 3000;
+
+	var onSiteLoaded = function () {
+		setTimeout(function () {
+			site.find(sels.ctn).each(function () {
+				var t = $(this);
+				if (!!t.attr('data-href') && !t.html()) {
+					$.ajax({
+						method: 'GET',
+						crossDomain: true,
+						url: t.attr('data-href'),
+						success: function (data) {
+							t.append($(data).find('watermark').html());
+						}
+					});
+				}
+			});
+		}, DELAY);
+	};
+
+	var actions = function () {
+		return {
+			site: {
+				loaded: onSiteLoaded
 			}
-		});
+		};
 	};
 
 	App.modules.exports('watermark', {
-		init: init
+		actions: actions
 	});
 
 })(jQuery);
